83798e558527c1c36fbee16ee260fa5e55e5c741
Baktiar Miah <baktiar.m@gmail.com>
2020-02-12 19:26:08 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205318_create_passengers.rb
+++ db/migrate/20140212205318_create_passengers.rb
@@ -2,6 +2,8 @@ class CreatePassengers < ActiveRecord::Migration
   def change
     create_table :passengers do |t|
       t.timestamps null: false
+      t.belongs_to :taxi
+      t.belongs_to :passenger
     end
   end
 end
