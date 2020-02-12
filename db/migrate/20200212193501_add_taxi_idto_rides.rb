626f377939bb0cc9e0854c67b4bfa554ef03f109
Baktiar Miah <baktiar.m@gmail.com>
2020-02-12 19:35:28 +0000

    Automatically backed up by Learn

--- db/migrate/20200212193501_add_taxi_idto_rides.rb
+++ db/migrate/20200212193501_add_taxi_idto_rides.rb
@@ -1,6 +1,7 @@
 class AddTaxiIdtoRides < ActiveRecord::Migration[5.0]
   def change
-    t.integer :taxi_id
-    t.integer :passenger_id
+    change_table :rides do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
   end
 end
